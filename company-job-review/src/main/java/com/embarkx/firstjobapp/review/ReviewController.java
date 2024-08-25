33b8bfd2ca0566e8c13f7911856fe6604a90aa07
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-08-24 20:36:11 -0700

    PUT and DELETE review

--- company-job-review/src/main/java/com/embarkx/firstjobapp/review/ReviewController.java
+++ company-job-review/src/main/java/com/embarkx/firstjobapp/review/ReviewController.java
@@ -32,4 +32,38 @@ public class ReviewController {
             return new ResponseEntity<>("Review Not Saved",
                     HttpStatus.NOT_FOUND);
     }
+
+    @GetMapping("/reviews/{reviewId}")
+    public ResponseEntity<Review> getReview(@PathVariable Long companyId,
+                                            @PathVariable Long reviewId){
+        return new ResponseEntity<>(reviewService.getReview(companyId, reviewId),
+                HttpStatus.OK);
+    }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> updateReview(@PathVariable Long companyId,
+                                               @PathVariable Long reviewId,
+                                               @RequestBody Review review){
+        boolean isReviewUpdated = reviewService.updateReview(companyId,
+                reviewId, review);
+        if (isReviewUpdated)
+            return new ResponseEntity<>("Review updated successfully",
+                    HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Review not updated",
+                    HttpStatus.NOT_FOUND);
+    }
+
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> deleteReview(@PathVariable Long companyId,
+                                               @PathVariable Long reviewId){
+        boolean isReviewDeleted = reviewService.deleteReview(companyId, reviewId);
+        if (isReviewDeleted)
+            return new ResponseEntity<>("Review deleted successfully",
+                    HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Review not deleted",
+                    HttpStatus.NOT_FOUND);
+    }
 }
--- company-job-review/src/main/java/com/embarkx/firstjobapp/review/impl/ReviewServiceImpl.java
+++ company-job-review/src/main/java/com/embarkx/firstjobapp/review/impl/ReviewServiceImpl.java
@@ -38,4 +38,42 @@ public class ReviewServiceImpl implements ReviewService {
             return false;
         }
     }
+
+    @Override
+    public Review getReview(Long companyId, Long reviewId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+
+        return reviews.stream()
+                .filter(review -> review.getId().equals(reviewId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public boolean updateReview(Long companyId, Long reviewId, Review updatedReview) {
+        if (companyService.getCompanyById(companyId) != null){
+            updatedReview.setCompany(companyService.getCompanyById(companyId));
+            updatedReview.setId(reviewId);
+            reviewRepository.save(updatedReview);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteReview(Long companyId, Long reviewId) {
+        if (companyService.getCompanyById(companyId) != null
+                && reviewRepository.existsById(reviewId)){
+            Review review = reviewRepository.findById(reviewId).orElse(null);
+            Company company = review.getCompany();
+            company.getReviews().remove(review);
+            review.setCompany(null);
+            companyService.updateCompany(company, companyId);
+            reviewRepository.deleteById(reviewId);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
