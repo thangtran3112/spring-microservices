cadcfbd89f949ec36987d8b40080e1d14a9f6725
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-09-04 23:50:48 -0700

    fixed container name and add rabbit mq

---
+++ microservices-gateway-lb/jobms/src/main/java/com/embarkx/jobms/job/impl/JobServiceImpl.java
@@ -0,0 +1,111 @@
+package com.embarkx.jobms.job.impl;
+
+import com.embarkx.jobms.job.Job;
+import com.embarkx.jobms.job.JobRepository;
+import com.embarkx.jobms.job.JobService;
+import com.embarkx.jobms.job.clients.CompanyClient;
+import com.embarkx.jobms.job.clients.ReviewClient;
+import com.embarkx.jobms.job.dto.JobDTO;
+import com.embarkx.jobms.job.external.Company;
+import com.embarkx.jobms.job.external.Review;
+import com.embarkx.jobms.job.mapper.JobMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class JobServiceImpl implements JobService {
+    JobRepository jobRepository;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    //Wiring happens automatically through the constructor
+    private CompanyClient companyClient;
+    private ReviewClient reviewClient;
+
+    // @Autowired automatically injects the JobRepository
+    public JobServiceImpl(JobRepository jobRepository, CompanyClient companyClient, ReviewClient reviewClient) {
+        this.companyClient = companyClient;
+        this.reviewClient = reviewClient;
+        this.jobRepository = jobRepository;
+    }
+
+    @Override
+//    @CircuitBreaker(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+//    @Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    public List<JobDTO> findAll() {
+        List<Job> jobs = jobRepository.findAll();
+        return jobs.stream().map(this::convertToDto).collect(Collectors.toList());
+    }
+
+    public List<String> companyBreakerFallback(Exception e) {
+        List<String> list = new ArrayList<>();
+        list.add("Dummy");
+        return list;
+    }
+
+    private JobDTO convertToDto(Job job) {
+        Company company = companyClient.getCompany(job.getCompanyId());
+
+        /*ResponseEntity<List<Review>> reviewResponse = restTemplate.exchange(
+                "http://REVIEW-SERVICE:8083/reviews?companyId=" + job.getCompanyId(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Review>>() {
+                });*/
+        List<Review> reviews = reviewClient.getReviews(job.getCompanyId());
+
+        // List<Review> reviews = reviewResponse.getBody();
+        JobDTO jobDTO = JobMapper.mapToJobDto(job, company, reviews);
+        return jobDTO;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobRepository.save(job);
+    }
+
+    @Override
+    public JobDTO getJobById(Long id) {
+        Job job = jobRepository.findById(id).orElse(null);
+        return convertToDto(job);
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+
+        // equivalent to if (jobOptional != null)
+        if (jobOptional.isPresent()) {
+            Job job = jobOptional.get();
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setMinSalary(updatedJob.getMinSalary());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setLocation(updatedJob.getLocation());
+            jobRepository.save(job);
+            return true;
+        }
+        return false;
+    }
+}
